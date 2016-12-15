4d31f3ec6faf3601b1be9bfbd9f7b798b3cd076d
Jose Garcia <jose.reyes@ubidots.com>
2016-12-15 10:52:40 -0500

    added new example for publish and subscribe on the same routine

---
+++ examples/subscribe_and_publish/subscribe_and_publish.ino
@@ -0,0 +1,59 @@
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include "UbidotsESPMQTT.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define TOKEN "....." // Your Ubidots TOKEN
+#define WIFINAME "....." //Your SSID
+#define WIFIPASS "....." // Your Wifi Pass
+#define MQTTCLIENTNAME "....." // Your MQTT Client Name, it must be unique so we recommend to choose a random ASCCI name
+
+
+Ubidots client(TOKEN, MQTTCLIENTNAME);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  if ((char)payload[0]=='1'){
+    digitalWrite(16, HIGH);
+  }
+  else{
+    digitalWrite(16, LOW);
+  }
+  Serial.println();
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  client.wifiConnection(WIFINAME, WIFIPASS);
+  client.begin(callback);
+  pinMode(16, OUTPUT);
+  client.ubidotsSubscribe("esp8266","temperature"); //Insert the dataSource and Variable's Labels
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()){
+      client.reconnect();
+      client.ubidotsSubscribe("esp8266","temperature"); //Insert the dataSource and Variable's Labels
+      }
+  client.add("stuff", 10);
+  client.ubidotsPublish("source1");
+  client.loop();
+  }
