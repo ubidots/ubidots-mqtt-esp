7d3a4eaf7f0762ff68d0e51e0f42d3f6d41b930b
Jose garcia <jota@pop-os.localdomain>
2020-09-10 16:07:06 -0500

    handler: Updating default clientId Updating default MQTT client ID to make it char array type instead of hexa byte type

--- src/UbidotsESPMQTT.cpp
+++ src/UbidotsESPMQTT.cpp
@@ -27,89 +27,63 @@ Modified and Maintened by: Jose Garcia - Ubidots Inc
 
 #include "UbidotsESPMQTT.h"
 
-
-Ubidots::Ubidots(char* token){
-    initialize(token, NULL);
+Ubidots::Ubidots(char* token) {
+  String _deviceMac = WiFi.macAddress();
+  _clientName = new char[_deviceMac.length() + 1];
+  strcpy(_clientName, _deviceMac.c_str());
+  initialize(token, _clientName);
 }
 
+Ubidots::Ubidots(char* token, char* clientName) { initialize(token, clientName); }
 
-Ubidots::Ubidots(char* token, char* clientName) {
-    initialize(token, clientName);
+void Ubidots::begin(void (*callback)(char*, uint8_t*, unsigned int)) {
+  this->callback = callback;
+  _client.setServer(_server, MQTT_PORT);
+  _client.setCallback(callback);
 }
 
+bool Ubidots::add(char* variableLabel, float value) { return add(variableLabel, value, "NULL", "NULL"); }
 
-void Ubidots::begin(void (*callback)(char*,uint8_t*,unsigned int)) {
-    this->callback = callback;
-    _client.setServer(_server, MQTT_PORT);
-    _client.setCallback(callback);
+bool Ubidots::add(char* variableLabel, float value, char* context) {
+  return add(variableLabel, value, context, "NULL");
 }
 
-
-bool Ubidots::add(char* variableLabel, float value) {
-    return add(variableLabel, value, "NULL", "NULL");
+bool Ubidots::add(char* variableLabel, float value, char* context, char* timestamp) {
+  (val + currentValue)->_variableLabel = variableLabel;
+  (val + currentValue)->_value = value;
+  (val + currentValue)->_context = context;
+  (val + currentValue)->_timestamp = timestamp;
+  currentValue++;
+  if (currentValue > MAX_VALUES) {
+    Serial.println(F("You are sending more than the maximum of consecutive variables"));
+    currentValue = MAX_VALUES;
+  }
+  return true;
 }
 
+bool Ubidots::connected() { return _client.connected(); }
 
-bool Ubidots::add(char* variableLabel, float value, char *context) {
-    return add(variableLabel, value, context, "NULL");
+void Ubidots::initialize(char* token, char* clientName) {
+  _server = SERVER;
+  _token = token;
+  currentValue = 0;
+  val = (Value*)malloc(MAX_VALUES * sizeof(Value));
+  _clientName = clientName;
 }
-
-
-bool Ubidots::add(char* variableLabel, float value, char *context, char *timestamp) {
-    (val+currentValue)->_variableLabel = variableLabel;
-    (val+currentValue)->_value = value;
-    (val+currentValue)->_context = context;
-    (val+currentValue)->_timestamp = timestamp;
-    currentValue++;
-    if (currentValue > MAX_VALUES) {
-        Serial.println(F("You are sending more than the maximum of consecutive variables"));
-        currentValue = MAX_VALUES;
-    }
-    return true;
-}
-
-
-bool Ubidots::connected(){
-    return _client.connected();
-}
-
-
-char* Ubidots::getMac(){
-    // Obtains the MAC of the device
-    Serial.println("entra");
-    byte mac[6];
-    WiFi.macAddress(mac);
-    char macAddr[18];
-    sprintf(macAddr, "%2X%2X%2X%2X%2X%2X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    return macAddr;
-}
-
-
-void Ubidots::initialize(char* token, char* clientName){
-    _server = SERVER;
-    _token = token;
-    currentValue = 0;
-    val = (Value *)malloc(MAX_VALUES*sizeof(Value));
-    if(clientName!=NULL){
-        _clientName = clientName;
-    }
-}
-
 
 bool Ubidots::loop() {
-    if (!_client.connected()) {
-        reconnect();
-    }
-    return _client.loop();
+  if (!_client.connected()) {
+    reconnect();
+  }
+  return _client.loop();
 }
 
-
 void Ubidots::reconnect() {
-    while (!_client.connected()) {
-        Serial.print("Attempting MQTT connection...");
-        if (_client.connect(_clientName, _token, NULL)) {
-            Serial.println("connected");
-            break;
+  while (!_client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (_client.connect(_clientName, _token, NULL)) {
+      Serial.println("connected");
+      break;
     } else {
       Serial.print("failed, rc=");
       Serial.print(_client.state());
@@ -119,80 +93,68 @@ void Ubidots::reconnect() {
   }
 }
 
+void Ubidots::setDebug(bool debug) { _debug = debug; }
 
-void Ubidots::setDebug(bool debug){
-    _debug = debug;
+void Ubidots::ubidotsSetBroker(char* broker) {
+  if (_debug) {
+    Serial.println("Broker set for Business Account");
+  }
+  _server = broker;
 }
-
-
-void Ubidots::ubidotsSetBroker(char* broker){
-    if (_debug){
-        Serial.println("Broker set for Business Account");
-    }
-    _server = broker;
-}
-
 
 bool Ubidots::ubidotsSubscribe(char* deviceLabel, char* variableLabel) {
-    char topic[150];
-    sprintf(topic, "%s%s/%s/lv", FIRST_PART_TOPIC, deviceLabel, variableLabel);
-    if (!_client.connected()) {
-        reconnect();
-    }
-    if (_debug){
-        Serial.println("Subscribed to: ");
-        Serial.println(topic);
-    }
-    return _client.subscribe(topic);
+  char topic[150];
+  sprintf(topic, "%s%s/%s/lv", FIRST_PART_TOPIC, deviceLabel, variableLabel);
+  if (!_client.connected()) {
+    reconnect();
+  }
+  if (_debug) {
+    Serial.println("Subscribed to: ");
+    Serial.println(topic);
+  }
+  return _client.subscribe(topic);
 }
 
-
-bool Ubidots::ubidotsPublish(char *deviceLabel) {
-    char topic[150];
-    char payload[500];
-    String str;
-    sprintf(topic, "%s%s", FIRST_PART_TOPIC, deviceLabel);
-    sprintf(payload, "{");
-    for (int i = 0; i <= currentValue; ) {
-        str = String((val+i)->_value, 2);
-        sprintf(payload, "%s\"%s\": [{\"value\": %s", payload, (val+i)->_variableLabel, str.c_str());
-        if ((val+i)->_timestamp != "NULL") {
-            sprintf(payload, "%s, \"timestamp\": %s", payload, (val+i)->_timestamp);
-        }
-        if ((val+i)->_context != "NULL") {
-            sprintf(payload, "%s, \"context\": {%s}", payload, (val+i)->_context);
-        }
-        i++;
-        if (i >= currentValue) {
-            sprintf(payload, "%s}]}", payload);
-            break;
-        } else {
-            sprintf(payload, "%s}], ", payload);
-        }
+bool Ubidots::ubidotsPublish(char* deviceLabel) {
+  char topic[150];
+  char payload[500];
+  String str;
+  sprintf(topic, "%s%s", FIRST_PART_TOPIC, deviceLabel);
+  sprintf(payload, "{");
+  for (int i = 0; i <= currentValue;) {
+    str = String((val + i)->_value, 2);
+    sprintf(payload, "%s\"%s\": [{\"value\": %s", payload, (val + i)->_variableLabel, str.c_str());
+    if ((val + i)->_timestamp != "NULL") {
+      sprintf(payload, "%s, \"timestamp\": %s", payload, (val + i)->_timestamp);
     }
-    if (_debug){
-        Serial.println("publishing to TOPIC: ");
-        Serial.println(topic);
-        Serial.print("JSON dict: ");
-        Serial.println(payload);
+    if ((val + i)->_context != "NULL") {
+      sprintf(payload, "%s, \"context\": {%s}", payload, (val + i)->_context);
     }
-    currentValue = 0;
-    return _client.publish(topic, payload, 512);
+    i++;
+    if (i >= currentValue) {
+      sprintf(payload, "%s}]}", payload);
+      break;
+    } else {
+      sprintf(payload, "%s}], ", payload);
+    }
+  }
+  if (_debug) {
+    Serial.println("publishing to TOPIC: ");
+    Serial.println(topic);
+    Serial.print("JSON dict: ");
+    Serial.println(payload);
+  }
+  currentValue = 0;
+  return _client.publish(topic, payload, 512);
 }
-
 
 bool Ubidots::wifiConnection(char* ssid, char* pass) {
-    WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println(F("WiFi connected"));
-    Serial.println(F("IP address: "));
-    Serial.println(WiFi.localIP());
-    if(_clientName==NULL){
-        _clientName = getMac();
-    }
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.println(WiFi.localIP());
 }
-
-
--- src/UbidotsESPMQTT.h
+++ src/UbidotsESPMQTT.h
@@ -27,58 +27,56 @@ Modified by: Jose Garcia
 
 #ifndef UbidotsESPMQTT_H
 #define UbidotsESPMQTT_H
-#include "PubSubClient.h"
 #include <ESP8266WiFi.h>
 
+#include "PubSubClient.h"
+#include "string.h"
+
 #define MQTT_PORT 1883
-#define SERVER "industrial.api.ubidots.com"
+#define SERVER "things.ubidots.com"
 #define MAX_VALUES 5
 #define FIRST_PART_TOPIC "/v1.6/devices/"
 #define DEFAULT_DEVICE_LABEL "ESP8266"
 
-
 #define META_DEBUG Serial
 
 typedef struct Value {
-    char  *_variableLabel;
-    float _value;
-    char *_context;
-    char *_timestamp;
+  char* _variableLabel;
+  float _value;
+  char* _context;
+  char* _timestamp;
 } Value;
-
-
 
 class Ubidots {
  private:
-    void (*callback)(char*,uint8_t*,unsigned int);
-    char* getMac();
-    void initialize(char* token, char* clientName);
-    WiFiClient espClient;
-    PubSubClient _client = PubSubClient(espClient);
-    char* _clientName = NULL;
-    bool _debug = false;
-    uint8_t currentValue;
-    char* _password;
-    char* _server;
-    char* _ssid;
-    char* _token;
-    Value * val;
- 
+  void (*callback)(char*, uint8_t*, unsigned int);
+  void initialize(char* token, char* clientName);
+  WiFiClient espClient;
+  PubSubClient _client = PubSubClient(espClient);
+  char* _clientName;
+  bool _debug = false;
+  uint8_t currentValue;
+  char* _password;
+  char* _server;
+  char* _ssid;
+  char* _token;
+  Value* val;
+
  public:
-    Ubidots(char* token);
-    Ubidots(char* token, char* clientName);
-    bool add(char* variableLabel, float value);
-    bool add(char* variableLabel, float value, char *context);
-    bool add(char* variableLabel, float value, char *context, char *timestamp);
-    void begin(void (*callback)(char*,uint8_t*,unsigned int));
-    bool connected();
-    bool loop();
-    bool ubidotsSubscribe(char* deviceLabel, char* variableLabel);
-    bool ubidotsPublish(char *deviceLabel);
-    void ubidotsSetBroker(char* broker);
-    void reconnect();
-    void setDebug(bool debug);
-    bool wifiConnection(char* ssid, char* pass);
+  Ubidots(char* token);
+  Ubidots(char* token, char* clientName);
+  bool add(char* variableLabel, float value);
+  bool add(char* variableLabel, float value, char* context);
+  bool add(char* variableLabel, float value, char* context, char* timestamp);
+  void begin(void (*callback)(char*, uint8_t*, unsigned int));
+  bool connected();
+  bool loop();
+  bool ubidotsSubscribe(char* deviceLabel, char* variableLabel);
+  bool ubidotsPublish(char* deviceLabel);
+  void ubidotsSetBroker(char* broker);
+  void reconnect();
+  void setDebug(bool debug);
+  bool wifiConnection(char* ssid, char* pass);
 };
 
 #endif
