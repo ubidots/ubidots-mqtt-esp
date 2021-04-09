3717aea590ac25aa987725689c53e331a0d825d4
jose <reyes2000jose@gmail.com>
2021-04-08 20:22:47 -0500

    Examples Adding example for non-blocking publish routine

---
+++ examples/publishNonBlocking/publishNonBlocking.ino
@@ -0,0 +1,69 @@
+/******************************************
+ *
+ * This example works for both Industrial and STEM users.
+ * If you are using the old educational platform,
+ * please consider to migrate your account to a STEM plan
+ *
+ * ****************************************/
+
+/****************************************
+ * Include Libraries
+ ****************************************/
+#include "UbidotsESPMQTT.h"
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define TOKEN "...."     // Your Ubidots TOKEN
+#define WIFINAME "...."  // Your SSID
+#define WIFIPASS "...."  // Your Wifi Pass
+
+Ubidots client(TOKEN);
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  client.setDebug(true);  // Pass a true or false bool value to activate debug messages
+  client.wifiConnection(WIFINAME, WIFIPASS);
+  client.begin(callback);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    bool conn = client.connect();
+    while (!conn) {
+      Serial.print(".");
+      delay(500);
+      conn = client.connect();
+    }
+  }
+
+  // Publish values to 2 different data sources
+
+  client.add("stuff", 10.2);  // Insert your variable Labels and the value to be sent
+  client.ubidotsPublish("source1");
+  client.add("stuff", 10.2);
+  client.add("more-stuff", 120.2);
+  client.ubidotsPublish("source2");
+  client.loop();
+  delay(5000);
+}
